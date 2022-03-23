3f1d35112b0ce5f6dafb0009cfcbc5bdfab2b9bb
brgcode <van.mele@arch.ethz.ch>
2022-03-23 14:35:48 +0100

    temp

--- version.py
+++ version.py
@@ -1,3 +1,9 @@
 import compas
+import compas_cgal
+import compas_occ
+import compas_view2
 
 print(compas.__version__)
+print(compas_cgal.__version__)
+print(compas_occ.__version__)
+print(compas_view2.__version__)
